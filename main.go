f8517f18661785fec2e80402ce583cd820251801
rowinf <964049+rowinf@users.noreply.github.com>
2024-11-15 04:43:47 +1300

    initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"htmgopocketbase/__htmgo"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/maddalax/htmgo/framework/config"
+	"github.com/maddalax/htmgo/framework/h"
+	"github.com/maddalax/htmgo/framework/service"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	locator := service.NewLocator()
+	cfg := config.Get()
+	app := pocketbase.New()
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	h.Start(h.AppOpts{
+		ServiceLocator: locator,
+		LiveReload:     true,
+		Register: func(app *h.App) {
+			sub, err := fs.Sub(GetStaticAssets(), "assets/dist")
+			if err != nil {
+				panic(err)
+			}
+
+			http.FileServerFS(sub)
+
+			// change this in htmgo.yml (public_asset_path)
+			app.Router.Handle(fmt.Sprintf("%s/*", cfg.PublicAssetPath),
+				http.StripPrefix(cfg.PublicAssetPath, http.FileServerFS(sub)))
+
+			__htmgo.Register(app.Router)
+		},
+	})
+}
---
+++ pages/index.go
@@ -0,0 +1,23 @@
+package pages
+
+import (
+	"github.com/maddalax/htmgo/framework/h"
+	"htmgopocketbase/partials"
+)
+
+func IndexPage(ctx *h.RequestContext) *h.Page {
+	return RootPage(
+		h.Div(
+			h.Class("flex flex-col gap-4 items-center pt-24 min-h-screen bg-neutral-100"),
+			h.H3(
+				h.Id("intro-text"),
+				h.Text("hello htmgo"),
+				h.Class("text-5xl"),
+			),
+			h.Div(
+				h.Class("mt-3"),
+				partials.CounterForm(0),
+			),
+		),
+	)
+}
