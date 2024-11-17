6edef3b42ff1f2164abf9c210b07218f9b066765
rowinf <964049+rowinf@users.noreply.github.com>
2024-11-18 10:13:48 +1300

    try to fix invocation of htmgo server

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.0
 
 require (
 	github.com/go-chi/chi/v5 v5.1.0
-	github.com/maddalax/htmgo/framework v1.0.3-0.20241109183230-b234ead96499
+	github.com/maddalax/htmgo/framework v1.0.5
 	github.com/pocketbase/pocketbase v0.22.25
 )
 
--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"htmgopocketbase/__htmgo"
 	"io/fs"
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
+	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/maddalax/htmgo/framework/config"
-	"github.com/maddalax/htmgo/framework/h"
+	h "github.com/maddalax/htmgo/framework/h"
 	"github.com/maddalax/htmgo/framework/service"
 
 	"github.com/pocketbase/pocketbase"
@@ -17,22 +21,100 @@ import (
 	"github.com/pocketbase/pocketbase/core"
 )
 
-func main() {
-	locator := service.NewLocator()
-	cfg := config.Get()
-	app := pocketbase.New()
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+type AppOpts struct {
+	LiveReload     bool
+	ServiceLocator *service.Locator
+	Register       func(app *App)
+}
+
+type App struct {
+	Opts   AppOpts
+	Router *chi.Mux
+}
+
+const RequestContextKey = "htmgo.request.context"
+
+func (app *App) start() {
+	slog.SetLogLoggerLevel(h.GetLogLevel())
+
+	pb := pocketbase.New()
+	pb.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
 	})
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
+	// app.Router.Use(func(h http.Handler) http.Handler {
+	// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		cc := &hg.RequestContext{
+	// 			locator:  app.Opts.ServiceLocator,
+	// 			Request:  r,
+	// 			Response: w,
+	// 			kv:       make(map[string]interface{}),
+	// 		}
+	// 		populateHxFields(cc)
+	// 		ctx := context.WithValue(r.Context(), RequestContextKey, cc)
+	// 		h.ServeHTTP(w, r.WithContext(ctx))
+	// 	})
+	// })
+
+	if app.Opts.Register != nil {
+		app.Opts.Register(app)
 	}
 
-	h.Start(h.AppOpts{
+	// if app.Opts.LiveReload && h.IsDevelopment() {
+	// 	app.AddLiveReloadHandler("/dev/livereload")
+	// }
+
+	port := ":3000"
+	slog.Info(fmt.Sprintf("Server started at localhost%s", port))
+
+	if err := pb.Start(); err != nil {
+		slog.Error("failed to start pocketbase", err)
+		panic(err)
+	}
+	if err := http.ListenAndServe(port, app.Router); err != nil {
+		// If we are in watch mode, just try to kill any processes holding that port
+		// and try again
+		if h.IsDevelopment() && h.IsWatchMode() {
+			slog.Info("Port already in use, trying to kill the process and start again")
+			if runtime.GOOS == "windows" {
+				cmd := exec.Command("cmd", "/C", fmt.Sprintf(`for /F "tokens=5" %%i in ('netstat -aon ^| findstr :%s') do taskkill /F /PID %%i`, port))
+				cmd.Run()
+			} else {
+				cmd := exec.Command("bash", "-c", fmt.Sprintf("kill -9 $(lsof -ti%s)", port))
+				cmd.Run()
+			}
+
+			time.Sleep(time.Millisecond * 50)
+
+			// Try to start server again
+			if err := http.ListenAndServe(port, app.Router); err != nil {
+				slog.Error("Failed to restart server", "error", err)
+				panic(err)
+			}
+		}
+
+		panic(err)
+	}
+}
+
+// Start starts the htmgo server
+func start(opts AppOpts) {
+	router := chi.NewRouter()
+	instance := App{
+		Opts:   opts,
+		Router: router,
+	}
+	instance.start()
+}
+
+func main() {
+	locator := service.NewLocator()
+	cfg := config.Get()
+
+	start(AppOpts{
 		ServiceLocator: locator,
 		LiveReload:     true,
-		Register: func(app *h.App) {
+		Register: func(app *App) {
 			sub, err := fs.Sub(GetStaticAssets(), "assets/dist")
 			if err != nil {
 				panic(err)
